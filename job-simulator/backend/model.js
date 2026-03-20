259ec01e057903e70434f5a4dcb52ca03e9c5f38
LuispeFigueroa <fig24087@uvg.edu.gt>
2026-03-19 21:15:38 -0600

    arreglar dokerfiles y configuracion de model

--- job-simulator/backend/model.js
+++ job-simulator/backend/model.js
@@ -1,6 +1,7 @@
 export const validateData = (data) => {
     const { campo1, campo2, campo3, campo4, campo5, campo6 } = data;
 
+    // validar que todos existan
     if (
         campo1 === undefined ||
         campo2 === undefined ||
@@ -11,12 +12,25 @@ export const validateData = (data) => {
     ) {
         return "Todos los campos son obligatorios";
     }
-    if (typeof campo1 != "string") return "Campo 1 debe ser un string";
-    if (typeof campo2 != "string") return "Campo 2 debe ser un string";
-    if (typeof campo3 != "string") return "Campo 3 debe ser un string";
-    if (typeof campo4 != "integer") return "Campo 4 debe ser un integer";
-    if (typeof campo5 != "float") return "Campo 5 debe ser un float";
-    if (typeof campo6 != "boolean") return "Campo 6 debe ser un boolean";
-    return null;
 
+    // validar strings
+    if (typeof campo1 !== "string") return "Campo 1 debe ser un string";
+    if (typeof campo2 !== "string") return "Campo 2 debe ser un string";
+    if (typeof campo3 !== "string") return "Campo 3 debe ser un string";
+
+    // validar números
+    if (typeof campo4 !== "number" || !Number.isInteger(campo4)) {
+        return "Campo 4 debe ser un integer";
+    }
+
+    if (typeof campo5 !== "number" || isNaN(campo5)) {
+        return "Campo 5 debe ser un float";
+    }
+
+    // validar boolean
+    if (typeof campo6 !== "boolean") {
+        return "Campo 6 debe ser un boolean";
+    }
+
+    return null;
 };
--- job-simulator/resources/Dockerfile.javascript
+++ job-simulator/resources/Dockerfile.javascript
@@ -2,13 +2,11 @@ FROM node:20-alpine
 
 WORKDIR /app
 
-# copiar solo backend
 COPY backend/package*.json ./
 RUN npm install
 
-# copiar código backend
 COPY backend .
 
 EXPOSE 8080
 
-CMD ["node", "src/index.js"]
+CMD ["node", "index.js"]
